726eb1bd77c5b907a750b322ee3e862ec441c9ec
John <indianajohn@github>
2019-09-17 09:58:30 -0700

    Added missing header.

---
+++ libs/MVS/SceneDensify.h
@@ -0,0 +1,101 @@
+/*
+* DepthMap.h
+*
+* Copyright (c) 2014-2015 SEACAVE
+*
+* Author(s):
+*
+*      cDc <cdc.seacave@gmail.com>
+*
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+* Additional Terms:
+*
+*      You are required to preserve legal notices and author attributions in
+*      that material or in the Appropriate Legal Notices displayed by works
+*      containing it.
+*/
+
+#ifndef _MVS_SCENEDENSIFY_H_
+#define _MVS_SCENEDENSIFY_H_
+
+// I N C L U D E S /////////////////////////////////////////////////
+
+#include "Common.h"
+#include "Scene.h"
+
+// S T R U C T S ///////////////////////////////////////////////////
+
+namespace MVS {
+// structure used to compute all depth-maps
+class DepthMapsData
+{
+public:
+	DepthMapsData(MVS::Scene& _scene);
+	~DepthMapsData();
+
+	bool SelectViews(MVS::IIndexArr& images, MVS::IIndexArr& imagesMap, MVS::IIndexArr& neighborsMap);
+	bool SelectViews(MVS::DepthData& depthData);
+	bool InitViews(MVS::DepthData& depthData, MVS::IIndex idxNeighbor, MVS::IIndex numNeighbors);
+	bool InitDepthMap(MVS::DepthData& depthData);
+	bool EstimateDepthMap(MVS::IIndex idxImage);
+
+	bool RemoveSmallSegments(MVS::DepthData& depthData);
+	bool GapInterpolation(MVS::DepthData& depthData);
+
+	bool FilterDepthMap(MVS::DepthData& depthData, const MVS::IIndexArr& idxNeighbors, bool bAdjust=true);
+	void FuseDepthMaps(MVS::PointCloud& pointcloud, bool bEstimateColor, bool bEstimateNormal);
+
+protected:
+	static void* STCALL ScoreDepthMapTmp(void*);
+	static void* STCALL EstimateDepthMapTmp(void*);
+	static void* STCALL EndDepthMapTmp(void*);
+
+public:
+  MVS::Scene& scene;
+
+  MVS::DepthDataArr arrDepthData;
+
+	// used internally to estimate the depth-maps
+	Image8U::Size prevDepthMapSize; // remember the size of the last estimated depth-map
+	Image8U::Size prevDepthMapSizeTrg; // ... same for target image
+  MVS::DepthEstimator::MapRefArr coords; // map pixel index to zigzag matrix coordinates
+  MVS::DepthEstimator::MapRefArr coordsTrg; // ... same for target image
+};
+
+struct DenseDepthMapData {
+  MVS::Scene& scene;
+  MVS::IIndexArr images;
+  MVS::IIndexArr neighborsMap;
+	DepthMapsData detphMaps;
+	volatile Thread::safe_t idxImage;
+	SEACAVE::EventQueue events; // internal events queue (processed by the working threads)
+	Semaphore sem;
+	CAutoPtr<Util::Progress> progress;
+
+	DenseDepthMapData(MVS::Scene& _scene)
+		: scene(_scene), detphMaps(_scene), idxImage(0), sem(1) {}
+
+	void SignalCompleteDepthmapFilter() {
+		ASSERT(idxImage > 0);
+		if (Thread::safeDec(idxImage) == 0)
+			sem.Signal((unsigned)images.GetSize()*2);
+	}
+};
+
+};
+/*----------------------------------------------------------------*/
+#endif
