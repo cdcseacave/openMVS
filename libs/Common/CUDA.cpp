22372e45b8fb1bff11a6f338ea5e4f25ebb59b84
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2019-10-18 19:22:51 +0530

    common: updated MapSMtoCores for SM 5.3 to SM 7.0 (#489)

--- libs/Common/CUDA.cpp
+++ libs/Common/CUDA.cpp
@@ -42,6 +42,11 @@ int _convertSMVer2Cores(int major, int minor)
 		{0x37, 192}, // Kepler Generation (SM 3.7) GK21x class
 		{0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class
 		{0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class
+		{0x53, 128}, // Maxwell Generation (SM 5.3) GM20x class
+        	{0x60, 64 }, // Pascal Generation (SM 6.0) GP100 class
+        	{0x61, 128}, // Pascal Generation (SM 6.1) GP10x class
+        	{0x62, 128}, // Pascal Generation (SM 6.2) GP10x class
+        	{0x70, 64 }, // Volta Generation (SM 7.0) GV100 class
 		{-1, -1}
 	};
 
