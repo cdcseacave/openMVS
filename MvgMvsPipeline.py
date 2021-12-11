c103ffa137984f4f2446ed6d2859c2a5f4514505
cDc <cdc.seacave@gmail.com>
2021-12-11 22:34:21 +0200

    interface: update MVG-MVS pipeline

--- MvgMvsPipeline.py
+++ MvgMvsPipeline.py
@@ -12,25 +12,28 @@ usage: MvgMvs_Pipeline.py [-h] [--steps STEPS [STEPS ...]] [--preset PRESET]
                           [--9 9 [9 ...]] [--10 10 [10 ...]] [--11 11 [11 ...]]
                           [--12 12 [12 ...]] [--13 13 [13 ...]]
                           [--14 14 [14 ...]] [--15 15 [15 ...]]
+                          [--16 16 [16 ...]] [--17 17 [17 ...]]
                           input_dir output_dir
 
 Photogrammetry reconstruction with these steps:
     0. Intrinsics analysis             openMVG_main_SfMInit_ImageListing
     1. Compute features                openMVG_main_ComputeFeatures
-    2. Compute matches                 openMVG_main_ComputeMatches
-    3. Incremental reconstruction      openMVG_main_IncrementalSfM
-    4. Global reconstruction           openMVG_main_GlobalSfM
-    5. Colorize Structure              openMVG_main_ComputeSfM_DataColor
-    6. Structure from Known Poses      openMVG_main_ComputeStructureFromKnownPoses
-    7. Colorized robust triangulation  openMVG_main_ComputeSfM_DataColor
-    8. Control Points Registration     ui_openMVG_control_points_registration
-    9. Export to openMVS               openMVG_main_openMVG2openMVS
-    10. Densify point-cloud            DensifyPointCloud
-    11. Reconstruct the mesh           ReconstructMesh
-    12. Refine the mesh                RefineMesh
-    13. Texture the mesh               TextureMesh
-    14. Estimate disparity-maps        DensifyPointCloud
-    15. Fuse disparity-maps            DensifyPointCloud
+    2. Compute pairs                   openMVG_main_PairGenerator
+    3. Compute matches                 openMVG_main_ComputeMatches
+    4. Filter matches                  openMVG_main_GeometricFilter
+    5. Incremental reconstruction      openMVG_main_IncrementalSfM
+    6. Global reconstruction           openMVG_main_GlobalSfM
+    7. Colorize Structure              openMVG_main_ComputeSfM_DataColor
+    8. Structure from Known Poses      openMVG_main_ComputeStructureFromKnownPoses
+    9. Colorized robust triangulation  openMVG_main_ComputeSfM_DataColor
+    10. Control Points Registration    ui_openMVG_control_points_registration
+    11. Export to openMVS              openMVG_main_openMVG2openMVS
+    12. Densify point-cloud            DensifyPointCloud
+    13. Reconstruct the mesh           ReconstructMesh
+    14. Refine the mesh                RefineMesh
+    15. Texture the mesh               TextureMesh
+    16. Estimate disparity-maps        DensifyPointCloud
+    17. Fuse disparity-maps            DensifyPointCloud
 
 positional arguments:
   input_dir                 the directory wich contains the pictures set.
@@ -40,12 +43,12 @@ optional arguments:
   -h, --help                show this help message and exit
   --steps STEPS [STEPS ...] steps to process
   --preset PRESET           steps list preset in
-                            SEQUENTIAL = [0, 1, 2, 3, 9, 10, 11, 12, 13]
-                            GLOBAL = [0, 1, 2, 4, 9, 10, 11, 12, 13]
-                            MVG_SEQ = [0, 1, 2, 3, 5, 6, 7]
-                            MVG_GLOBAL = [0, 1, 2, 4, 5, 6, 7]
-                            MVS = [10, 11, 12, 13]
-                            MVS_SGM = [14, 15]
+                            SEQUENTIAL = [0, 1, 2, 3, 4, 5, 11, 12, 13, 14, 15]
+                            GLOBAL = [0, 1, 2, 3, 4, 6, 11, 12, 13, 14, 15]
+                            MVG_SEQ = [0, 1, 2, 3, 4, 5, 7, 8, 9]
+                            MVG_GLOBAL = [0, 1, 2, 3, 4, 6, 7, 8, 9]
+                            MVS = [11, 12, 13, 14, 15]
+                            MVS_SGM = [16, 17]
                             default : SEQUENTIAL
 
 Passthrough:
@@ -114,12 +117,12 @@ if not CAMERA_SENSOR_DB_DIRECTORY:
     CAMERA_SENSOR_DB_DIRECTORY = input("openMVG camera database (%s) folder?\n" % CAMERA_SENSOR_DB_FILE)
 
 
-PRESET = {'SEQUENTIAL': [0, 1, 2, 3, 9, 10, 11, 12, 13],
-          'GLOBAL': [0, 1, 2, 4, 9, 10, 11, 12, 13],
-          'MVG_SEQ': [0, 1, 2, 3, 5, 6, 7],
-          'MVG_GLOBAL': [0, 1, 2, 4, 5, 6, 7],
-          'MVS': [10, 11, 12, 13],
-          'MVS_SGM': [14, 15]}
+PRESET = {'SEQUENTIAL': [0, 1, 2, 3, 4, 5, 11, 12, 13, 14, 15],
+          'GLOBAL': [0, 1, 2, 3, 4, 6, 11, 12, 13, 14, 15],
+          'MVG_SEQ': [0, 1, 2, 3, 4, 5, 7, 8, 9],
+          'MVG_GLOBAL': [0, 1, 2, 3, 4, 6, 7, 8, 9],
+          'MVS': [11, 12, 13, 14, 15],
+          'MVS_SGM': [16, 17]}
 
 PRESET_DEFAULT = 'SEQUENTIAL'
 
@@ -186,46 +189,52 @@ class StepsStore:
             ["Compute features",             # 1
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeFeatures"),
              ["-i", "%matches_dir%/sfm_data.json", "-o", "%matches_dir%", "-m", "SIFT"]],
-            ["Compute matches",              # 2
+            ["Compute pairs",                # 2
+             os.path.join(OPENMVG_BIN, "openMVG_main_PairGenerator"),
+             ["-i", "%matches_dir%/sfm_data.json", "-o", "%matches_dir%/pairs.bin"]],
+            ["Compute matches",              # 3
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeMatches"),
-             ["-i", "%matches_dir%/sfm_data.json", "-o", "%matches_dir%", "-n", "AUTO"]],
-            ["Incremental reconstruction",   # 3
-             os.path.join(OPENMVG_BIN, "openMVG_main_IncrementalSfM"),
-             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%"]],
-            ["Global reconstruction",        # 4
-             os.path.join(OPENMVG_BIN, "openMVG_main_GlobalSfM"),
-             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%"]],
-            ["Colorize Structure",           # 5
+             ["-i", "%matches_dir%/sfm_data.json", "-p", "%matches_dir%/pairs.bin", "-o", "%matches_dir%/matches.putative.bin", "-n", "AUTO"]],
+            ["Filter matches",               # 4
+             os.path.join(OPENMVG_BIN, "openMVG_main_GeometricFilter"),
+             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%/matches.putative.bin", "-o", "%matches_dir%/matches.f.bin"]],
+            ["Incremental reconstruction",   # 5
+             os.path.join(OPENMVG_BIN, "openMVG_main_SfM"),
+             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%", "-s", "INCREMENTAL"]],
+            ["Global reconstruction",        # 6
+             os.path.join(OPENMVG_BIN, "openMVG_main_SfM"),
+             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%", "-s", "GLOBAL", "-M", "%matches_dir%/matches.e.bin"]],
+            ["Colorize Structure",           # 7
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeSfM_DataColor"),
              ["-i", "%reconstruction_dir%/sfm_data.bin", "-o", "%reconstruction_dir%/colorized.ply"]],
-            ["Structure from Known Poses",   # 6
+            ["Structure from Known Poses",   # 8
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeStructureFromKnownPoses"),
              ["-i", "%reconstruction_dir%/sfm_data.bin", "-m", "%matches_dir%", "-f", "%matches_dir%/matches.f.bin", "-o", "%reconstruction_dir%/robust.bin"]],
-            ["Colorized robust triangulation",  # 7
+            ["Colorized robust triangulation",  # 9
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeSfM_DataColor"),
              ["-i", "%reconstruction_dir%/robust.bin", "-o", "%reconstruction_dir%/robust_colorized.ply"]],
-            ["Control Points Registration",  # 8
+            ["Control Points Registration",  # 10
              os.path.join(OPENMVG_BIN, "ui_openMVG_control_points_registration"),
              ["-i", "%reconstruction_dir%/sfm_data.bin"]],
-            ["Export to openMVS",            # 9
+            ["Export to openMVS",            # 11
              os.path.join(OPENMVG_BIN, "openMVG_main_openMVG2openMVS"),
              ["-i", "%reconstruction_dir%/sfm_data.bin", "-o", "%mvs_dir%/scene.mvs", "-d", "%mvs_dir%/images"]],
-            ["Densify point cloud",          # 10
+            ["Densify point cloud",          # 12
              os.path.join(OPENMVS_BIN, "DensifyPointCloud"),
              ["scene.mvs", "--dense-config-file", "Densify.ini", "--resolution-level", "1", "-w", "%mvs_dir%"]],
-            ["Reconstruct the mesh",         # 11
+            ["Reconstruct the mesh",         # 13
              os.path.join(OPENMVS_BIN, "ReconstructMesh"),
              ["scene_dense.mvs", "-w", "%mvs_dir%"]],
-            ["Refine the mesh",              # 12
+            ["Refine the mesh",              # 14
              os.path.join(OPENMVS_BIN, "RefineMesh"),
              ["scene_dense_mesh.mvs", "--scales", "2", "-w", "%mvs_dir%"]],
-            ["Texture the mesh",             # 13
+            ["Texture the mesh",             # 15
              os.path.join(OPENMVS_BIN, "TextureMesh"),
              ["scene_dense_mesh_refine.mvs", "--decimate", "0.5", "-w", "%mvs_dir%"]],
-            ["Estimate disparity-maps",      # 14
+            ["Estimate disparity-maps",      # 16
              os.path.join(OPENMVS_BIN, "DensifyPointCloud"),
              ["scene.mvs", "--dense-config-file", "Densify.ini", "--fusion-mode", "-1", "-w", "%mvs_dir%"]],
-            ["Fuse disparity-maps",          # 15
+            ["Fuse disparity-maps",          # 17
              os.path.join(OPENMVS_BIN, "DensifyPointCloud"),
              ["scene.mvs", "--dense-config-file", "Densify.ini", "--fusion-mode", "-2", "-w", "%mvs_dir%"]]
             ]
@@ -324,10 +333,11 @@ print("# Using input dir:  %s" % CONF.input_dir)
 print("#      output dir:  %s" % CONF.output_dir)
 print("# Steps:  %s" % str(CONF.steps))
 
-if 2 in CONF.steps:    # ComputeMatches
-    if 4 in CONF.steps:  # GlobalReconstruction
+if 4 in CONF.steps:    # GeometricFilter
+    if 6 in CONF.steps:  # GlobalReconstruction
         # Set the geometric_model of ComputeMatches to Essential
-        STEPS[2].opt.extend(["-g", "e"])
+        STEPS[4].opt = STEPS[4].opt.replace("/matches.f.bin", "/matches.e.bin")
+        STEPS[4].opt.extend(["-g", "e"])
 
 for cstep in CONF.steps:
     printout("#%i. %s" % (cstep, STEPS[cstep].info), effect=INVERSE)
