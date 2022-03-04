3bbd042b0505777eda91840fa0fbd68aecc3ff23
cDc <cdc.seacave@gmail.com>
2022-03-04 14:45:31 +0200

    common: improve MvgMvsPipeline.py to support partial reconstruction steps

--- MvgMvsPipeline.py
+++ MvgMvsPipeline.py
@@ -55,12 +55,13 @@ Passthrough:
   Option to be passed to command lines (remove - in front of option names)
   e.g. --1 p ULTRA to use the ULTRA preset in openMVG_main_ComputeFeatures
   For example, running the script
-  [MvgMvsPipeline.py input_dir output_dir --1 p HIGH n 8 --2 n ANNL2]
+  [MvgMvsPipeline.py input_dir output_dir --steps 0 1 2 3 4 5 11 12 13 15 --1 p HIGH n 8 --3 n HNSWL2]
+  [--steps 0 1 2 3 4 5 11 12 13 15] runs only the desired steps
   [--1 p HIGH n 8] where --1 refer to openMVG_main_ComputeFeatures,
-  p refers to describerPreset option which HIGH was chosen, and n refers
-  to numThreads which 8 was used. The second step (Compute matches),
-  [--2 n ANNL2] where --2 refer to openMVG_main_ComputeMatches,
-  n refers to nearest_matching_method option which ANNL2 was chosen
+  p refers to describerPreset option and set to HIGH, and n refers
+  to numThreads and set to 8. The second step (Compute matches),
+  [--3 n HNSWL2] where --3 refer to openMVG_main_ComputeMatches,
+  n refers to nearest_matching_method option and set to HNSWL2
 """
 
 import os
@@ -72,8 +73,10 @@ DEBUG = False
 
 if sys.platform.startswith('win'):
     PATH_DELIM = ';'
+    FOLDER_DELIM = '\\'
 else:
     PATH_DELIM = ':'
+    FOLDER_DELIM = '/'
 
 # add this script's directory to PATH
 os.environ['PATH'] += PATH_DELIM + os.path.dirname(os.path.abspath(__file__))
@@ -195,37 +198,37 @@ class StepsStore:
              ["-i", "%input_dir%", "-o", "%matches_dir%", "-d", "%camera_file_params%"]],
             ["Compute features",             # 1
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeFeatures"),
-             ["-i", "%matches_dir%/sfm_data.json", "-o", "%matches_dir%", "-m", "SIFT"]],
+             ["-i", "%matches_dir%"+FOLDER_DELIM+"sfm_data.json", "-o", "%matches_dir%", "-m", "SIFT"]],
             ["Compute pairs",                # 2
              os.path.join(OPENMVG_BIN, "openMVG_main_PairGenerator"),
-             ["-i", "%matches_dir%/sfm_data.json", "-o", "%matches_dir%/pairs.bin"]],
+             ["-i", "%matches_dir%"+FOLDER_DELIM+"sfm_data.json", "-o", "%matches_dir%"+FOLDER_DELIM+"pairs.bin"]],
             ["Compute matches",              # 3
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeMatches"),
-             ["-i", "%matches_dir%/sfm_data.json", "-p", "%matches_dir%/pairs.bin", "-o", "%matches_dir%/matches.putative.bin", "-n", "AUTO"]],
+             ["-i", "%matches_dir%"+FOLDER_DELIM+"sfm_data.json", "-p", "%matches_dir%"+FOLDER_DELIM+"pairs.bin", "-o", "%matches_dir%"+FOLDER_DELIM+"matches.putative.bin", "-n", "AUTO"]],
             ["Filter matches",               # 4
              os.path.join(OPENMVG_BIN, "openMVG_main_GeometricFilter"),
-             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%/matches.putative.bin", "-o", "%matches_dir%/matches.f.bin"]],
+             ["-i", "%matches_dir%"+FOLDER_DELIM+"sfm_data.json", "-m", "%matches_dir%"+FOLDER_DELIM+"matches.putative.bin", "-o", "%matches_dir%"+FOLDER_DELIM+"matches.f.bin"]],
             ["Incremental reconstruction",   # 5
              os.path.join(OPENMVG_BIN, "openMVG_main_SfM"),
-             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%", "-s", "INCREMENTAL"]],
+             ["-i", "%matches_dir%"+FOLDER_DELIM+"sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%", "-s", "INCREMENTAL"]],
             ["Global reconstruction",        # 6
              os.path.join(OPENMVG_BIN, "openMVG_main_SfM"),
-             ["-i", "%matches_dir%/sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%", "-s", "GLOBAL", "-M", "%matches_dir%/matches.e.bin"]],
+             ["-i", "%matches_dir%"+FOLDER_DELIM+"sfm_data.json", "-m", "%matches_dir%", "-o", "%reconstruction_dir%", "-s", "GLOBAL", "-M", "%matches_dir%"+FOLDER_DELIM+"matches.e.bin"]],
             ["Colorize Structure",           # 7
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeSfM_DataColor"),
-             ["-i", "%reconstruction_dir%/sfm_data.bin", "-o", "%reconstruction_dir%/colorized.ply"]],
+             ["-i", "%reconstruction_dir%"+FOLDER_DELIM+"sfm_data.bin", "-o", "%reconstruction_dir%"+FOLDER_DELIM+"colorized.ply"]],
             ["Structure from Known Poses",   # 8
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeStructureFromKnownPoses"),
-             ["-i", "%reconstruction_dir%/sfm_data.bin", "-m", "%matches_dir%", "-f", "%matches_dir%/matches.f.bin", "-o", "%reconstruction_dir%/robust.bin"]],
+             ["-i", "%reconstruction_dir%"+FOLDER_DELIM+"sfm_data.bin", "-m", "%matches_dir%", "-f", "%matches_dir%"+FOLDER_DELIM+"matches.f.bin", "-o", "%reconstruction_dir%"+FOLDER_DELIM+"robust.bin"]],
             ["Colorized robust triangulation",  # 9
              os.path.join(OPENMVG_BIN, "openMVG_main_ComputeSfM_DataColor"),
-             ["-i", "%reconstruction_dir%/robust.bin", "-o", "%reconstruction_dir%/robust_colorized.ply"]],
+             ["-i", "%reconstruction_dir%"+FOLDER_DELIM+"robust.bin", "-o", "%reconstruction_dir%"+FOLDER_DELIM+"robust_colorized.ply"]],
             ["Control Points Registration",  # 10
              os.path.join(OPENMVG_BIN, "ui_openMVG_control_points_registration"),
-             ["-i", "%reconstruction_dir%/sfm_data.bin"]],
+             ["-i", "%reconstruction_dir%"+FOLDER_DELIM+"sfm_data.bin"]],
             ["Export to openMVS",            # 11
              os.path.join(OPENMVG_BIN, "openMVG_main_openMVG2openMVS"),
-             ["-i", "%reconstruction_dir%/sfm_data.bin", "-o", "\"%mvs_dir%/scene.mvs\"", "-d", "\"%mvs_dir%/images\""]],
+             ["-i", "%reconstruction_dir%"+FOLDER_DELIM+"sfm_data.bin", "-o", "%mvs_dir%"+FOLDER_DELIM+"scene.mvs", "-d", "%mvs_dir%"+FOLDER_DELIM+"images"]],
             ["Densify point cloud",          # 12
              os.path.join(OPENMVS_BIN, "DensifyPointCloud"),
              ["scene.mvs", "--dense-config-file", "Densify.ini", "--resolution-level", "1", "--number-views", "8", "-w", "\"%mvs_dir%\""]],
@@ -266,6 +269,15 @@ class StepsStore:
                 o2.append(co)
             s[2] = o2
 
+    def replace_opt(self, idx, str_exist, str_new):
+        """ replace each existing str_exist with str_new per opt value in step idx data """
+        s = self.steps_data[idx]
+        o2 = []
+        for o in s[2]:
+            co = o.replace(str_exist, str_new)
+            o2.append(co)
+        s[2] = o2
+
 
 CONF = ConfContainer()
 STEPS = StepsStore()
@@ -289,7 +301,7 @@ PARSER.add_argument('--preset',
                     " \r\n".join([k + " = " + str(PRESET[k]) for k in PRESET]) +
                     " \r\ndefault : " + PRESET_DEFAULT)
 
-GROUP = PARSER.add_argument_group('Passthrough', description="Option to be passed to command lines (remove - in front of option names)\r\ne.g. --1 p ULTRA to use the ULTRA preset in openMVG_main_ComputeFeatures\r\nFor example, running the script as follows,\r\nMvgMvsPipeline.py input_dir output_dir --1 p HIGH n 8 --2 n ANNL2\r\nwhere --1 refer to openMVG_main_ComputeFeatures, p refers to\r\ndescriberPreset option which HIGH was chosen, and n refers to\r\nnumThreads which 8 was used. --2 refer to second step (openMVG_main_ComputeMatches),\r\nn refers to nearest_matching_method option which ANNL2 was chosen")
+GROUP = PARSER.add_argument_group('Passthrough', description="Option to be passed to command lines (remove - in front of option names)\r\ne.g. --1 p ULTRA to use the ULTRA preset in openMVG_main_ComputeFeatures\r\nFor example, running the script as follows,\r\nMvgMvsPipeline.py input_dir output_dir --1 p HIGH n 8 --3 n ANNL2\r\nwhere --1 refer to openMVG_main_ComputeFeatures, p refers to\r\ndescriberPreset option which HIGH was chosen, and n refers to\r\nnumThreads which 8 was used. --3 refer to second step (openMVG_main_ComputeMatches),\r\nn refers to nearest_matching_method option which ANNL2 was chosen")
 for n in range(STEPS.length()):
     GROUP.add_argument('--'+str(n), nargs='+')
 
@@ -343,8 +355,13 @@ print("# Steps:  %s" % str(CONF.steps))
 if 4 in CONF.steps:    # GeometricFilter
     if 6 in CONF.steps:  # GlobalReconstruction
         # Set the geometric_model of ComputeMatches to Essential
-        STEPS[4].opt = STEPS[4].opt.replace("/matches.f.bin", "/matches.e.bin")
+        STEPS.replace_opt(4, FOLDER_DELIM+"matches.f.bin", FOLDER_DELIM+"matches.e.bin")
         STEPS[4].opt.extend(["-g", "e"])
+
+if 15 in CONF.steps:    # TextureMesh
+    if 14 not in CONF.steps:  # RefineMesh
+        # RefineMesh step is not run, use ReconstructMesh output
+        STEPS.replace_opt(15, "scene_dense_mesh_refine.mvs", "scene_dense_mesh.mvs")
 
 for cstep in CONF.steps:
     printout("#%i. %s" % (cstep, STEPS[cstep].info), effect=INVERSE)
