90a45b6ebbc81db41bff80ccf75a87937c773141
Yan Ke <yke@yanke.org>
2023-03-13 16:09:30 -0700

    Unify Dockerfile w/ and w/o CUDA.

--- docker/buildFromScratch_CUDA.sh
+++ docker/buildFromScratch_CUDA.sh
@@ -1,2 +1,2 @@
-docker build --no-cache -t="openmvs-ubuntu" --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --file Dockerfile_CUDA .;
+docker build --no-cache -t="openmvs-ubuntu-cuda" --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg CUDA=1 --build-arg BASE_IMAGE=nvidia/cuda:11.8.0-devel-ubuntu22.04 .;
 docker run --gpus=all --entrypoint bash -w /work -v $1:/work -it openmvs-ubuntu;
