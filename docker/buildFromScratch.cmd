3f9eeef51d239572823d215ceeae42496a1e4349
cDc <cdc.seacave@gmail.com>
2023-04-08 19:13:55 +0300

    fix user and add gui param

--- docker/buildFromScratch.cmd
+++ docker/buildFromScratch.cmd
@@ -28,17 +28,22 @@ if "%~1" == "--workspace" (
   shift
   goto parse_args
 )
+if "%~1" == "--gui" (
+  set "XSOCK=\\.\pipe\X11-unix"
+  set "XAUTH=%TEMP%\docker.xauth"
+  set "DISPLAY_ARGS=-e ^"DISPLAY=%COMPUTERNAME%:0.0^" -v ^"%XAUTH%:%XAUTH%:rw^" -e ^"XAUTHORITY=%XAUTH%^""
+  shift
+  goto parse_args
+)
 echo Unknown argument: %~1
 exit /b 1
 
 :build_image
 echo Running with workspace: %WORKSPACE%
 
-docker build -t="openmvs-ubuntu%CUDA_CONTAINER_SUFFIX%" --build-arg "USER_ID=%USERPROFILE:~-4%" --build-arg "GROUP_ID=%USERPROFILE:~-4%" %CUDA_BUILD_ARGS% %MASTER_ARGS% .
+docker build -t="openmvs-ubuntu%CUDA_CONTAINER_SUFFIX%" --build-arg "USER_ID=1001" --build-arg "GROUP_ID=1001" %CUDA_BUILD_ARGS% %MASTER_ARGS% .
 if errorlevel 1 exit /b 1
 
-set "XSOCK=\\.\pipe\X11-unix"
-set "XAUTH=%TEMP%\docker.xauth"
-docker run %CUDA_RUNTIME_ARGS% --entrypoint bash --ipc=host --shm-size=4gb -w /work -v "%WORKSPACE%:/work" -e "DISPLAY=%COMPUTERNAME%:0.0" -v "%XAUTH%:%XAUTH%:rw" -e "XAUTHORITY=%XAUTH%" -it openmvs-ubuntu%CUDA_CONTAINER_SUFFIX%
+docker run %CUDA_RUNTIME_ARGS% --entrypoint bash --ipc=host --shm-size=4gb -w /work -v "%WORKSPACE%:/work" %DISPLAY_ARGS% -it openmvs-ubuntu%CUDA_CONTAINER_SUFFIX%
 
 exit /b 0
