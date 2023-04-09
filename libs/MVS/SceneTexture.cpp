f19dc348d0edfb3bc416a159078599a712140d38
Popa Bogdan-Gabriel(PxGluz) <badddog25@gmail.com>
2023-04-09 18:18:42 +0300

    updated floodfill

--- libs/MVS/SceneTexture.cpp
+++ libs/MVS/SceneTexture.cpp
@@ -519,20 +519,20 @@ bool MeshTexture::ListCameraFaces(FaceDataViewArr& facesDatas, float fOutlierThr
 		RasterMesh rasterer(vertices, imageData.camera, depthMap, faceMap);
 		// creating mask for the image border
 		rasterer.mask = Image8U(imageData.height + 2, imageData.width + 2);
-		rasterer.mask = rasterer.mask != rasterer.mask;
+		rasterer.mask.memset(0);
 		Image8U imageCopy;
+		cv::Rect rect;
 		cv::cvtColor(imageData.image, imageCopy, cv::COLOR_BGR2GRAY);
-		imageCopy -= 1; // this line gets rid of white spots in the middle of the image
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(0, 0), 255);
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(0, imageCopy.rows / 2), 255);
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(0, imageCopy.rows - 1), 255);
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols / 2, imageCopy.rows - 1), 255);
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols - 1, imageCopy.rows - 1), 255);
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols - 1, imageCopy.rows / 2), 255);
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols - 1, 0), 255);
-		cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols / 2, 0), 255);
+		if (imageCopy(0, 0) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(0, 0), 255, &rect, cv::Scalar(0), cv::Scalar(1));
+		if (imageCopy(imageCopy.rows / 2, 0) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(0, imageCopy.rows / 2), 255, &rect, cv::Scalar(0), cv::Scalar(1));
+		if (imageCopy(imageCopy.rows - 1, 0) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(0, imageCopy.rows - 1), 255, &rect, cv::Scalar(0), cv::Scalar(1));
+		if (imageCopy(imageCopy.rows - 1, imageCopy.cols / 2) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols / 2, imageCopy.rows - 1), 255, &rect, cv::Scalar(0), cv::Scalar(1));
+		if (imageCopy(imageCopy.rows - 1, imageCopy.cols - 1) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols - 1, imageCopy.rows - 1), 255, &rect, cv::Scalar(0), cv::Scalar(1));
+		if (imageCopy(imageCopy.rows / 2, imageCopy.cols - 1) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols - 1, imageCopy.rows / 2), 255, &rect, cv::Scalar(0), cv::Scalar(1));
+		if (imageCopy(0, imageCopy.cols - 1) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols - 1, 0), 255, &rect, cv::Scalar(0), cv::Scalar(1));
+		if (imageCopy(0, imageCopy.cols / 2) == 0) cv::floodFill(imageCopy, rasterer.mask, cv::Point(imageCopy.cols / 2, 0), 255, &rect, cv::Scalar(0), cv::Scalar(1));
 		cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
-		cv::erode(rasterer.mask, rasterer.mask, kernel);
+		//cv::erode(rasterer.mask, rasterer.mask, kernel);
 		rasterer.mask = rasterer.mask == 0;
 		if (VERBOSITY_LEVEL > 2) {
 			cv::imwrite(String::FormatString("invalidMask%d.png", idx), rasterer.mask);
