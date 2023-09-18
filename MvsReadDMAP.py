061fa9ee4b18c0aaa39e971d991644fd7210a4bd
4CJ7T <108303308+4CJ7T@users.noreply.github.com>
2023-09-17 20:06:54 -0700

    Update MvsReadDMAP.py

--- MvsReadDMAP.py
+++ MvsReadDMAP.py
@@ -1,5 +1,5 @@
 '''
-This script is an example for reading .dmap files created by the DensifyPointCloud module.
+This script is an example for reading .dmap files.
 
 usage: MvsReadDMAP.py [-h] [--input INPUT]
 '''
@@ -15,8 +15,17 @@ def loadDMAP(dmap_path):
     content_type = struct.unpack('B', dmap.read(1))[0]
     reserve = struct.unpack('B', dmap.read(1))[0]
     
+    has_depth = content_type > 0
+    has_normal = content_type in [3, 7, 11, 15]
+    has_conf = content_type in [6, 7, 13, 15]
+    has_views = content_type in [9, 11, 13, 15]
+    
     image_width, image_height = struct.unpack('2I', dmap.read(8))
     depth_width, depth_height = struct.unpack('2I', dmap.read(8))
+    
+    if (file_type != 'DR' or has_depth == False or depth_width <= 0 or depth_height <= 0 or image_width < depth_width or image_height < depth_height):
+      print('error: opening file \'%s\' for reading depth-data' % dmap_path)
+      return None
     
     depth_min, depth_max = struct.unpack('2f', dmap.read(8))
     
@@ -32,9 +41,9 @@ def loadDMAP(dmap_path):
     
     depth_length = depth_width * depth_height
     depth_map = np.asarray(struct.unpack('%df' % depth_length, dmap.read(4 * depth_length))).reshape(depth_height, depth_width)
-    normal_map = np.asarray(struct.unpack('%df' % depth_length * 3, dmap.read(4 * depth_length * 3))).reshape(depth_height, depth_width, 3)
-    confidence_map = np.asarray(struct.unpack('%df' % depth_length, dmap.read(4 * depth_length))).reshape(depth_height, depth_width)
-    views_map = np.asarray(struct.unpack('%dB' % depth_length * 4, dmap.read(depth_length * 4))).reshape(depth_height, depth_width, 4)
+    normal_map = np.asarray(struct.unpack('%df' % depth_length * 3, dmap.read(4 * depth_length * 3))).reshape(depth_height, depth_width, 3) if has_normal else None
+    confidence_map = np.asarray(struct.unpack('%df' % depth_length, dmap.read(4 * depth_length))).reshape(depth_height, depth_width) if has_conf else None
+    views_map = np.asarray(struct.unpack('%dB' % depth_length * 4, dmap.read(depth_length * 4))).reshape(depth_height, depth_width, 4) if has_views else None
   
   data = {
     'image_width': image_width,
@@ -63,9 +72,12 @@ def main():
   
   dmap = loadDMAP(args.input)
   
-  pyvips.Image.new_from_array(np.uint8(dmap['depth_map'] * (1 / dmap['depth_max']) * 255)).write_to_file('depth_map.png')
-  pyvips.Image.new_from_array(np.uint8((dmap['normal_map'] @ -dmap['R'].T + 1) * 0.5 * 255)).write_to_file('normal_map.png')
-  pyvips.Image.new_from_array(np.uint8(dmap['confidence_map'] * (1 / dmap['confidence_map'].max()) * 255)).write_to_file('confidence_map.png')
+  try:
+    pyvips.Image.new_from_array(np.uint8(dmap['depth_map'] * (1 / dmap['depth_max']) * 255)).write_to_file('depth_map.png')
+    pyvips.Image.new_from_array(np.uint8((dmap['normal_map'] @ -dmap['R'].T + 1) * 0.5 * 255)).write_to_file('normal_map.png')
+    pyvips.Image.new_from_array(np.uint8(dmap['confidence_map'] * (1 / dmap['confidence_map'].max()) * 255)).write_to_file('confidence_map.png')
+  except:
+    pass
 
 if __name__ == '__main__':
   main()
