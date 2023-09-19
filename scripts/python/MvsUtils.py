ab2693b974ad61bf1879b6fc7f7c87b9de487599
4CJ7T <108303308+4CJ7T@users.noreply.github.com>
2023-09-18 20:53:53 -0700

    Update MvsUtils.py

--- scripts/python/MvsUtils.py
+++ scripts/python/MvsUtils.py
@@ -5,43 +5,42 @@ E.g., from MvsUtils import loadDMAP
 '''
 
 import numpy as np
-import struct
 
 def loadDMAP(dmap_path):
   with open(dmap_path, 'rb') as dmap:
     file_type = dmap.read(2).decode()
-    content_type = struct.unpack('B', dmap.read(1))[0]
-    reserve = struct.unpack('B', dmap.read(1))[0]
+    content_type = np.frombuffer(dmap.read(1), dtype=np.dtype('B'))
+    reserve = np.frombuffer(dmap.read(1), dtype=np.dtype('B'))
     
     has_depth = content_type > 0
     has_normal = content_type in [3, 7, 11, 15]
     has_conf = content_type in [5, 7, 13, 15]
     has_views = content_type in [9, 11, 13, 15]
     
-    image_width, image_height = struct.unpack('2I', dmap.read(8))
-    depth_width, depth_height = struct.unpack('2I', dmap.read(8))
+    image_width, image_height = np.frombuffer(dmap.read(8), dtype=np.dtype('I'))
+    depth_width, depth_height = np.frombuffer(dmap.read(8), dtype=np.dtype('I'))
     
     if (file_type != 'DR' or has_depth == False or depth_width <= 0 or depth_height <= 0 or image_width < depth_width or image_height < depth_height):
       print('error: opening file \'%s\' for reading depth-data' % dmap_path)
       return None
     
-    depth_min, depth_max = struct.unpack('2f', dmap.read(8))
+    depth_min, depth_max = np.frombuffer(dmap.read(8), dtype=np.dtype('f'))
     
-    file_name_length = struct.unpack('H', dmap.read(2))[0]
+    file_name_length = np.frombuffer(dmap.read(2), dtype=np.dtype('H'))[0]
     file_name = dmap.read(file_name_length).decode()
     
-    view_ids_length = struct.unpack('I', dmap.read(4))[0]
-    view_ids = np.asarray(struct.unpack('%dI' % view_ids_length, dmap.read(4 * view_ids_length)))
+    view_ids_length = np.frombuffer(dmap.read(4), dtype=np.dtype('I'))[0]
+    view_ids = np.frombuffer(dmap.read(4 * view_ids_length), dtype=np.dtype('I'))
     
-    K = np.asarray(struct.unpack('9d', dmap.read(72))).reshape(3, 3)
-    R = np.asarray(struct.unpack('9d', dmap.read(72))).reshape(3, 3)
-    C = np.asarray(struct.unpack('3d', dmap.read(24)))
+    K = np.frombuffer(dmap.read(72), dtype=np.dtype('d')).reshape(3, 3)
+    R = np.frombuffer(dmap.read(72), dtype=np.dtype('d')).reshape(3, 3)
+    C = np.frombuffer(dmap.read(24), dtype=np.dtype('d'))
     
     depth_length = depth_width * depth_height
-    depth_map = np.asarray(struct.unpack('%df' % depth_length, dmap.read(4 * depth_length))).reshape(depth_height, depth_width)
-    normal_map = np.asarray(struct.unpack('%df' % depth_length * 3, dmap.read(4 * depth_length * 3))).reshape(depth_height, depth_width, 3) if has_normal else np.asarray([])
-    confidence_map = np.asarray(struct.unpack('%df' % depth_length, dmap.read(4 * depth_length))).reshape(depth_height, depth_width) if has_conf else np.asarray([])
-    views_map = np.asarray(struct.unpack('%dB' % depth_length * 4, dmap.read(depth_length * 4))).reshape(depth_height, depth_width, 4) if has_views else np.asarray([])
+    depth_map = np.frombuffer(dmap.read(4 * depth_length), dtype=np.dtype('f')).reshape(depth_height, depth_width)
+    normal_map = np.frombuffer(dmap.read(4 * depth_length * 3), dtype=np.dtype('f')).reshape(depth_height, depth_width, 3) if has_normal else np.asarray([])
+    confidence_map = np.frombuffer(dmap.read(4 * depth_length), dtype=np.dtype('f')).reshape(depth_height, depth_width) if has_conf else np.asarray([])
+    views_map = np.frombuffer(dmap.read(depth_length * 4), dtype=np.dtype('B')).reshape(depth_height, depth_width, 4) if has_views else np.asarray([])
   
   data = {
     'has_normal': has_normal,
