7b9d3486f55161fc80ea95e7c6ea3043416be9e7
4CJ7T <108303308+4CJ7T@users.noreply.github.com>
2023-10-13 20:34:50 -0700

    Update MvsUtils.py

--- scripts/python/MvsUtils.py
+++ scripts/python/MvsUtils.py
@@ -2,6 +2,7 @@
 OpenMVS python utilities.
 
 E.g., from MvsUtils import loadDMAP
+E.g., from MvsUtils import loadMVSInterface
 '''
 
 import numpy as np
@@ -21,8 +22,8 @@ def loadDMAP(dmap_path):
     depth_width, depth_height = np.frombuffer(dmap.read(8), dtype=np.dtype('I'))
     
     if (file_type != 'DR' or has_depth == False or depth_width <= 0 or depth_height <= 0 or image_width < depth_width or image_height < depth_height):
-      print('error: opening file \'%s\' for reading depth-data' % dmap_path)
-      return None
+      print('error: opening file \'{}\' for reading depth-data'.format(dmap_path))
+      return
     
     depth_min, depth_max = np.frombuffer(dmap.read(8), dtype=np.dtype('f'))
     
@@ -63,5 +64,125 @@ def loadDMAP(dmap_path):
     'confidence_map': confidence_map,
     'views_map': views_map
   }
+  
+  return data
+
+def loadMVSInterface(archive_path):
+  with open(archive_path, 'rb') as mvs:
+    archive_type = mvs.read(4).decode()
+    version = np.frombuffer(mvs.read(4), dtype=np.dtype('I')).tolist()[0]
+    reserve = np.frombuffer(mvs.read(4), dtype=np.dtype('I'))
+    
+    if archive_type != 'MVSI':
+      print('error: opening file \'{}\''.format(archive_path))
+      return
+    
+    data = {
+      'project_stream': archive_type,
+      'project_stream_version': version,
+      'platforms': [],
+      'images': [],
+      'vertices': [],
+      'vertices_normal': [],
+      'vertices_color': []
+    }
+    
+    platforms_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+    for platform_index in range(platforms_size):
+      platform_name_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+      platform_name = mvs.read(platform_name_size).decode()
+      data['platforms'].append({'name': platform_name, 'cameras': []})
+      cameras_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+      for camera_index in range(cameras_size):
+        camera_name_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+        camera_name = mvs.read(camera_name_size).decode()
+        data['platforms'][platform_index]['cameras'].append({'name': camera_name})
+        if version > 3:
+          band_name_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+          band_name = mvs.read(band_name_size).decode()
+          data['platforms'][platform_index]['cameras'][camera_index].update({'band_name': band_name})
+        if version > 0:
+          width, height = np.frombuffer(mvs.read(8), dtype=np.dtype('I')).tolist()
+          data['platforms'][platform_index]['cameras'][camera_index].update({'width': width, 'height': height})
+        K = np.asarray(np.frombuffer(mvs.read(72), dtype=np.dtype('d'))).reshape(3, 3).tolist()
+        data['platforms'][platform_index]['cameras'][camera_index].update({'K': K, 'poses': []})
+        identity_matrix = np.asarray(np.frombuffer(mvs.read(96), dtype=np.dtype('d'))).reshape(4, 3)
+        poses_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+        for _ in range(poses_size):
+          R = np.asarray(np.frombuffer(mvs.read(72), dtype=np.dtype('d'))).reshape(3, 3).tolist()
+          C = np.asarray(np.frombuffer(mvs.read(24), dtype=np.dtype('d'))).tolist()
+          data['platforms'][platform_index]['cameras'][camera_index]['poses'].append({'R': R, 'C': C})
+    
+    images_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+    for image_index in range(images_size):
+      name_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+      name = mvs.read(name_size).decode()
+      data['images'].append({'name': name})
+      if version > 4:
+        mask_name_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+        mask_name = mvs.read(mask_name_size).decode()
+        data['images'][image_index].update({'mask_name': mask_name})
+      platform_id, camera_id, pose_id = np.frombuffer(mvs.read(12), dtype=np.dtype('I')).tolist()
+      data['images'][image_index].update({'platform_id': platform_id, 'camera_id': camera_id, 'pose_id': pose_id})
+      if version > 2:
+        id = np.frombuffer(mvs.read(4), dtype=np.dtype('I')).tolist()[0]
+        data['images'][image_index].update({'id': id})
+      if version > 6:
+        min_depth, avg_depth, max_depth = np.frombuffer(mvs.read(12), dtype=np.dtype('f')).tolist()
+        data['images'][image_index].update({'min_depth': min_depth, 'avg_depth': avg_depth, 'max_depth': max_depth, 'view_scores': []})
+        view_score_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+        for _ in range(view_score_size):
+          view_score_id, view_score_points = np.frombuffer(mvs.read(8), dtype=np.dtype('I')).tolist()
+          view_score_scale, view_score_angle, view_score_area, view_score_score = np.frombuffer(mvs.read(16), dtype=np.dtype('f')).tolist()
+          data['images'][image_index]['view_scores'].append({'id': view_score_id, 'points': view_score_points, 'scale': view_score_scale, 'angle': view_score_angle, 'area': view_score_area, 'score': view_score_score})
+    
+    vertices_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+    for vertex_index in range(vertices_size):
+      X = np.frombuffer(mvs.read(12), dtype=np.dtype('f')).tolist()
+      data['vertices'].append({'X': X, 'views': []})
+      views_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+      for _ in range(views_size):
+        image_id = np.frombuffer(mvs.read(4), dtype=np.dtype('I')).tolist()[0]
+        confidence = np.frombuffer(mvs.read(4), dtype=np.dtype('f')).tolist()[0]
+        data['vertices'][vertex_index]['views'].append({'image_id': image_id, 'confidence': confidence})
+    
+    vertices_normal_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+    for _ in range(vertices_normal_size):
+      normal = np.frombuffer(mvs.read(12), dtype=np.dtype('f')).tolist()
+      data['vertices_normal'].append(normal)
+    
+    vertices_color_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+    for _ in range(vertices_color_size):
+      color = np.frombuffer(mvs.read(3), dtype=np.dtype('B')).tolist()
+      data['vertices_color'].append(color)
+    
+    if version > 0:
+      data.update({'lines': [], 'lines_normal': [], 'lines_color': []})
+      lines_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+      for line_index in range(lines_size):
+        pt1 = np.frombuffer(mvs.read(12), dtype=np.dtype('f')).tolist()
+        pt2 = np.frombuffer(mvs.read(12), dtype=np.dtype('f')).tolist()
+        data['lines'].append({'pt1': pt1, 'pt2': pt2, 'views': []})
+        views_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+        for _ in range(views_size):
+          image_id = np.frombuffer(mvs.read(4), dtype=np.dtype('I')).tolist()[0]
+          confidence = np.frombuffer(mvs.read(4), dtype=np.dtype('f')).tolist()[0]
+          data['lines'][line_index]['views'].append({'image_id': image_id, 'confidence': confidence})
+      lines_normal_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+      for _ in range(lines_normal_size):
+        normal = np.frombuffer(mvs.read(12), dtype=np.dtype('f')).tolist()
+        data['lines_normal'].append(normal)
+      lines_color_size = np.frombuffer(mvs.read(8), dtype=np.dtype('Q'))[0]
+      for _ in range(lines_color_size):
+        color = np.frombuffer(mvs.read(3), dtype=np.dtype('B')).tolist()
+        data['lines_color'].append(color)
+      if version > 1:
+        transform = np.frombuffer(mvs.read(128), dtype=np.dtype('d')).reshape(4, 4).tolist()
+        data.update({'transform': transform})
+        if version > 5:
+          rot = np.frombuffer(mvs.read(72), dtype=np.dtype('d')).reshape(3, 3).tolist()
+          pt_min = np.frombuffer(mvs.read(24), dtype=np.dtype('d')).tolist()
+          pt_max = np.frombuffer(mvs.read(24), dtype=np.dtype('d')).tolist()
+          data.update({'obb': {'rot': rot, 'pt_min': pt_min, 'pt_max': pt_max}})
   
   return data
